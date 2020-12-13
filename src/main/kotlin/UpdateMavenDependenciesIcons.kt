b4f4d1e84509dc32874a29ad2966d70a65c518de
Sergei Tachenov <sergei@tachenov.name>
2020-12-13 16:04:04 +0300

    Fix deprecation warning for IconLoader.getIcon
    Replace with two arguments overload.

--- src/main/kotlin/UpdateMavenDependenciesIcons.kt
+++ src/main/kotlin/UpdateMavenDependenciesIcons.kt
@@ -1,9 +1,11 @@
 package icons
 
-import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.util.*
+import name.tachenov.intellij.plugins.mavenDependencyUpdater.*
 
 object UpdateMavenDependenciesIcons {
 
-    @JvmField val UPDATE_MAVEN_DEPENDENCIES = IconLoader.getIcon("/icons/UpdateMavenDependencies.png")
+    @JvmField val UPDATE_MAVEN_DEPENDENCIES = IconLoader.getIcon("/icons/UpdateMavenDependencies.png",
+            UpdateMavenDependencies::class.java)
 
 }
